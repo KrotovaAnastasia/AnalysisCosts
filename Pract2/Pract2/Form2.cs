41681e441187db29353ef849beef580fb9d1260d
AnikinaDaria <dasha.anik.al@gmail.com>
2020-12-18 00:08:34 +0400

    Реализован дизайн графика

--- Pract2/Pract2/Form2.cs
+++ Pract2/Pract2/Form2.cs
@@ -19,7 +19,7 @@ namespace Pract2
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            //gfjkf
+            //gfjkffjgf
         }
 
         private void справкаToolStripMenuItem1_Click(object sender, EventArgs e)
